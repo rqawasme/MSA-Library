c92ea961fa93fe754cdb21ad0c1b20d7090f7336
rqawasme <rqawasme@sfu.ca>
2022-09-19 18:50:51 -0700

    key revoked

--- msalibrary/settings.py
+++ msalibrary/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(#mc*axn7cli)@+w#_o)jswc=ocotp%40q94wezfo+9(bf_31n'
+SECRET_KEY = 
+# 'django-insecure-(#mc*axn7cli)@+w#_o)jswc=ocotp%40q94wezfo+9(bf_31n' #This is invalid now make sure to use the real one
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
