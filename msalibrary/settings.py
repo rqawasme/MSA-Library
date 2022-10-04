f3cb0826027f1a7c037d3c5921200756011b8743
rqawasme <rqawasme@sfu.ca>
2022-10-04 02:20:35 -0700

    updated allowed hosts

--- msalibrary/settings.py
+++ msalibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 1234
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['library.sfumsa.ca', 'localhost', '127.0.0.1']
 
 
 # Application definition
