6ca4f7e50855fcae099b9f6d9befaf74c4954c48
rqawasme <rqawasme@sfu.ca>
2022-10-09 20:32:30 -0700

    why not commit these files, maybe it will work

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ pip install -r requirements.txt
 
 python manage.py migrate
 python manage.py loaddata superuser_fixture.json
-python manage.py collectstatic --clear
+# python manage.py collectstatic --clear
 python manage.py collectstatic
 python3 manage.py findstatic pages/css/styles.css
 # python manage.py collectstatic --no-input
--- msalibrary/settings.py
+++ msalibrary/settings.py
@@ -152,7 +152,7 @@ STATICFILES_FINDERS = [
 ]
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "/opt/render/project/src/static/"# os.path.join(BASE_DIR, 'static')
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATICFILES_DIRS = [
