1b17787c8963c80adb78191247a18ca995e5d887
rqawasme <rqawasme@sfu.ca>
2022-11-15 11:04:43 -0800

    increased name length in signup

--- accounts/views.py
+++ accounts/views.py
@@ -13,17 +13,17 @@ username_validator = UnicodeUsernameValidator()
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=50, help_text='Required. Enter a valid email address.',  required=True,
                              widget=(forms.TextInput(attrs={'class': 'form-control'})))
-    first_name = forms.CharField(max_length=12, min_length=4, required=True, help_text='Required: First Name',
+    first_name = forms.CharField(max_length=20, min_length=2, required=True, help_text='Required: First Name',
                                 widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
-    last_name = forms.CharField(max_length=12, min_length=4, required=True, help_text='Required: Last Name',
+    last_name = forms.CharField(max_length=20, min_length=2, required=True, help_text='Required: Last Name',
                                widget=(forms.TextInput(attrs={'class': 'form-control'})))
-    phone_number = forms.CharField(max_length=20, help_text='Required. Enter a valid phone number.', required=True,
+    phone_number = forms.CharField(max_length=20, min_length=7, help_text='Required. Enter a valid phone number.', required=True,
                              widget=(forms.TextInput(attrs={'class': 'form-control'})))
     password1 = forms.CharField(label=_('Password'), required=False,
                                 widget=(forms.PasswordInput(attrs={'class': 'form-control'})),
                                 help_text=password_validation.password_validators_help_text_html())
     password2 = forms.CharField(label=_('Password Confirmation'), widget=forms.PasswordInput(attrs={'class': 'form-control'}), required=False,
-                                help_text=_('Just Enter the same password, for confirmation'))
+                                help_text=_('Enter the same password, for confirmation'))
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'phone_number', 'password1', 'password2',)
