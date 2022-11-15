da55f09431784ed35195e7a7e205af32424c9db3
Rashid Qawasmeh <rqawasmeh55@gmail.com>
2022-11-14 19:00:34 -0800

    password not required

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from accounts.models import User
+
+# Register out own model admin, based on the default UserAdmin
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = [ 'id', 'first_name', 'last_name', 'email', 'phone_number', 'is_staff']
