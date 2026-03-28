5324ffe8c9bb494b58dd5c6bdac1a3e1f62aeeea
Abdul aziz Hamoui <abdul_aziz_hamoui@sfu.ca>
2026-03-27 21:45:58 -0700

    Added books data

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py loaddata superuser_fixture.json
+python manage.py loaddata books_fixture.json
 python manage.py collectstatic
 python manage.py findstatic pages/css/styles.css
 # python manage.py crontab add
